8c3584a08ab5f811d0f0d674fdda3b7d1698635e
T-Midnight <BelousovaTanya1995@yandex.ru>
2013-11-16 07:17:21 +0400

    first

---
+++ MatrixMultiplication/MatrixMultiplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+//using System.Text;
+
+namespace MatrixMultiplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите размер матриц: ");
+            int size = Int32.Parse(Console.ReadLine());
+            int[,] Nonidentity = new int[size, size];
+            int[,] Identity = new int[size, size];
+
+            for (int i = 0; i < Nonidentity.Length; i++) {
+                for (int j = 0; j < Nonidentity.Length; j++) {
+                    Random rand = new Random();
+                    Nonidentity[i, j] = rand.Next(0, 9);
+                    Console.Write(Nonidentity[i, j]);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
